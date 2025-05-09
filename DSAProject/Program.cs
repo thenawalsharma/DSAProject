65067a58c8bbd0358294e44e67d6d0e5c69d6617
NawalKiPoojA <NawalKiPoojA@NAWAL>
2025-05-09 22:23:13 +0530

    Add project files.

---
+++ DSAProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+class Program
+{
+   static void Main(string[] args)
+    {
+        int[] arr = new int[5];
+        for(int i=0;i<arr.Length; i++)
+        {
+            Console.WriteLine("Enter the number");
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        for(int i = 0; i < arr.Length; i++)
+        {
+            Console.WriteLine("The number is: " + arr[i]);
+        }
+    }
+}
