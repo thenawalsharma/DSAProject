d66f0bc346f9d3bbc82fcf8e9a0a3eade8fa8a0e
NawalSharma <thenawalsharma@gmail.com>
2025-05-19 23:00:23 +0530

    Constructor Code Example

---
+++ DSAProject/Car.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAProject
+{
+    public class Car
+    {
+        public string Brand;
+        public string Model;
+        public static string Company;
+
+
+        // Default Constructor
+        public Car()
+        {
+            Brand = "DefaultBrand";
+            Model = "DefaultModel";
+            Console.WriteLine("Default Constructor called");
+        }
+
+        // Parameterized Constructor
+        public Car(string brand, string model)
+        {
+            Brand = brand;
+            Model = model;
+            Console.WriteLine("Parameterized Constructor called");
+        }
+
+        // Copy Constructor
+        public Car(Car existingCar)
+        {
+            Brand = existingCar.Brand;
+            Model = existingCar.Model;
+            Console.WriteLine("Copy Constructor called");
+        }
+
+        // Private Constructor
+        private Car(string message)
+        {
+            Console.WriteLine("Private Constructor: " + message);
+        }
+
+        // Static method to access private constructor
+        public static Car CreateSpecialCar()
+        {
+            return new Car("Special Car created internally");
+        }
+
+
+        // Static Constructor
+        static Car()
+        {
+            Company = "Mercedes-Benz";
+            Console.WriteLine("Static Constructor called");
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Brand: {Brand}, Model: {Model}, Company: {Company}");
+        }
+    }
+}
--- DSAProject/Program.cs
+++ DSAProject/Program.cs
@@ -1,18 +1,23 @@
-﻿using System.Diagnostics;
+﻿using DSAProject;
+using System.Diagnostics;
 
 class Program
 {
    static void Main(string[] args)
     {
-        int[] arr = new int[5];
-        for(int i=0;i<arr.Length; i++)
-        {
-            Console.WriteLine("Enter the number");
-            arr[i] = Convert.ToInt32(Console.ReadLine());
-        }
-        for(int i = 0; i < arr.Length; i++)
-        {
-            Console.WriteLine("The number is: " + arr[i]);
-        }
+        Console.WriteLine("\n--- Default Constructor ---");
+        Car car1 = new Car();
+        car1.Display();
+
+        Console.WriteLine("\n--- Parameterized Constructor ---");
+        Car car2 = new Car("BMW", "X5");
+        car2.Display();
+
+        Console.WriteLine("\n--- Copy Constructor ---");
+        Car car3 = new Car(car2);
+        car3.Display();
+
+        Console.WriteLine("\n--- Private Constructor via Static Method ---");
+        Car special = Car.CreateSpecialCar();
     }
 }
